fa5f44f3842398a56a7522142d0ae8493f493200
Jonathan C. Lundstrom <jonathan@edsi.se>
2017-04-26 00:50:25 -0400

    Fixed Java Script syntax problems

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,23 +1,24 @@
-$.each($('.img-expandable'), function(i, val){
+
+$.each($('.img-expandable'), function (i, val) {
+    "use strict";
     var s = '';
-    if(i == 0)
-    {
-        s = '<div class="item img-expanded active"'
+    if (i === 0) {
+        s = '<div class="item img-expanded active"';
     } else {
         s = '<div class="item img-expanded"';
     }
     s += '><img class="img-responsive img-full" src="' + $(val).attr('expand') + '"><p>' + $(val).parent().siblings('.figure-caption').text() + '</p></div>';
     $(val).attr('expand-id', i);
-    $('#main-carousel').append(s)
+    $('#main-carousel').append(s);
 });
 
-$('.img-expand-button').on('click', function(event) {
+$('.img-expand-button').on('click', function (event) {
+    "use strict";
     event.preventDefault(); // To prevent following the link (optional)
 
-    var targetID = $(this).find('img').attr('expand-id')
-    $.each($('.img-expanded'), function(i, val){
-        if(i == targetID)
-        {
+    var targetID = parseInt($(this).find('img').attr('expand-id'), 10);
+    $.each($('.img-expanded'), function (i, val) {
+        if (i === targetID) {
             $(val).addClass("active");
         } else {
             $(val).removeClass("active");
